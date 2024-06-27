b0a93175891b7b9d97b625e146bdde28108c9a46
Non T <117605943+Ratanon691@users.noreply.github.com>
2024-06-27 12:47:46 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,3 @@
-// write code here
+const user = { name: 'Jack', role: 'ADMIN' };
+const admin = { name: 'Jack', role: 'ADMIN' };
+console.log(user === admin); //false เพราะ === ใช้กับobj จะเทียบจากreferenceของobjทั้งสองตัว ถ้าไม่ชี้ไปที่ตัวเดียวกันจะถือว่าfalse
